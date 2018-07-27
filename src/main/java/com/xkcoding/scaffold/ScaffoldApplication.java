e60bd393fe59647796cadaa672c5975393382d62
Yangkai.Shen <237497819@qq.com>
2018-07-27 14:37:30 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/xkcoding/scaffold/ScaffoldApplication.java
@@ -0,0 +1,25 @@
+package com.xkcoding.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @package: com.xkcoding.scaffold
+ * @description： 启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/7/27 下午2:36
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+public class ScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaffoldApplication.class, args);
+	}
+}

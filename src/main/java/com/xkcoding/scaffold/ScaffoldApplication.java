d0dc4394fc2a1540e4e35b59264a7a08c172f8cb
Yangkai.Shen <237497819@qq.com>
2018-08-01 10:56:25 +0800

    :loud_sound: Adding logback and change application name.

--- src/main/java/com/xkcoding/scaffold/ScaffoldApplication.java
+++ src/main/java/com/xkcoding/scaffold/ScaffoldApplication.java
@@ -21,6 +21,6 @@ public class ScaffoldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScaffoldApplication.class, args);
-		System.out.println("\n(♥◠‿◠)ﾉﾞ Scaffold 启动成功 ლ(´ڡ`ლ)ﾞ\n");
+		System.out.println("\n(♥◠‿◠)ﾉﾞ scaffold 启动成功 ლ(´ڡ`ლ)ﾞ\n");
 	}
 }

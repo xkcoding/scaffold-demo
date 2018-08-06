9ddfffb6a6794fa3a301afe59ccd88a46eb8f933
Yangkai.Shen <237497819@qq.com>
2018-08-06 18:01:06 +0800

    :white_check_mark: Adding password encode test.

---
+++ src/test/java/com/xkcoding/scaffold/user/PasswordEncoderTest.java
@@ -0,0 +1,39 @@
+package com.xkcoding.scaffold.user;
+
+import com.xkcoding.scaffold.ScaffoldApplicationTests;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * <p>
+ * 密码测试
+ * </p>
+ *
+ * @package: com.xkcoding.scaffold.com.xkcoding.scaffold.user
+ * @description： 密码测试
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/6 上午9:15
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Slf4j
+public class PasswordEncoderTest extends ScaffoldApplicationTests {
+
+	@Test
+	public void testGetPassword() {
+		String rawPassword = "123456" ;
+		String encodePassword1 = "$2a$10$NG9t0BI6ZxOuoB4GjQI8aeiD4KFbXH.bauTZTuquCjJpZnUeCq0fm" ;
+		String encodePassword2 = "$2a$10$OCGlv4ACVYO7aMearTHff.cqNmt1yJYujan7U4q2bGqYpEN3o5Bxy" ;
+		String encodePassword3 = "$2a$10$6N2Fn/HnV0hXiOeuJKEc3e/G.PIftVwmtmv48w8i467N1U0yPO0Ci" ;
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String encode = encoder.encode(rawPassword);
+
+		log.info(encode);
+		log.debug("does [{}] match [{}] ? " + "{}", rawPassword, encodePassword1, encoder.matches(rawPassword, encodePassword1));
+		log.debug("does [{}] match [{}] ? " + "{}", rawPassword, encodePassword2, encoder.matches(rawPassword, encodePassword2));
+		log.debug("does [{}] match [{}] ? " + "{}", rawPassword, encodePassword3, encoder.matches(rawPassword, encodePassword3));
+	}
+}

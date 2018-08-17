f2587ca3375ccf0efae92461dbb2aaaf5c0b5bec
Yangkai.Shen <237497819@qq.com>
2018-08-17 11:30:17 +0800

    :art: add common 404 NOT FOUND exception handler

--- src/main/java/com/xkcoding/scaffold/handler/GlobalExceptionHandler.java
+++ src/main/java/com/xkcoding/scaffold/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * <p>
@@ -29,13 +30,17 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(value = Exception.class)
 	@ResponseBody
 	public Api handlerException(Exception e) {
-		if (e instanceof ScaffoldException) {
+		if (e instanceof NoHandlerFoundException) {
+			log.error("【全局异常拦截】NoHandlerFoundException: 请求方法 {}, 请求路径 {}", ((NoHandlerFoundException) e).getRequestURL(), ((NoHandlerFoundException) e).getHttpMethod());
+			return Api.ofStatus(Status.REQUEST_NOT_FOUND);
+		} else if (e instanceof ScaffoldException) {
 			log.error("【全局异常拦截】ScaffoldException: 状态码 {}, 异常信息 {}", ((ScaffoldException) e).getCode(), e.getMessage());
 			return new Api(((ScaffoldException) e).getCode(), e.getMessage(), ((ScaffoldException) e).getData());
 		} else if (e instanceof AccessDeniedException) {
-			log.error("【全局异常拦截】AccessDeniedException: 异常信息 {}", ((AccessDeniedException) e).getMessage());
+			log.error("【全局异常拦截】AccessDeniedException: 异常信息 {}", e.getMessage());
 			return Api.ofStatus(Status.FORBIDDEN);
 		}
+
 		log.error("【全局异常拦截】: 异常信息 {} ", e.getMessage());
 		return Api.ofStatus(Status.INTERNAL_SERVER_ERROR);
 	}

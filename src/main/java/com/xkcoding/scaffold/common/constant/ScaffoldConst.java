d2b7912ff475c1a31793b2bc69aebeedc6f974a4
Yangkai.Shen <237497819@qq.com>
2018-08-17 11:31:02 +0800

    :art: Improving structure / format of the code.

---
+++ src/main/java/com/xkcoding/scaffold/common/constant/ScaffoldConst.java
@@ -0,0 +1,25 @@
+package com.xkcoding.scaffold.common.constant;
+
+/**
+ * <p>
+ * Scaffold 常量池
+ * </p>
+ *
+ * @package: com.xkcoding.scaffold.common.constant
+ * @description： Scaffold 常量池
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/17 上午10:05
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public interface ScaffoldConst {
+	/**
+	 * 逗号
+	 */
+	String COMMA = ",";
+	/**
+	 * 登录页面地址
+	 */
+	String AUTHENTICATION_LOGIN_PAGE = "/authentication/require";
+}
---
+++ src/main/java/com/xkcoding/scaffold/common/property/ScaffoldProperties.java
@@ -0,0 +1,20 @@
+package com.xkcoding.scaffold.common.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * <p>
+ * Scaffold配置类
+ * </p>
+ *
+ * @package: com.xkcoding.scaffold.common.property
+ * @description： Scaffold配置类
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/17 上午10:09
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@ConfigurationProperties(prefix = "scaffold")
+public class ScaffoldProperties {
+}
